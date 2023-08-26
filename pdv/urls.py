270fc31dadd02b44c2c0567da915df447ea90b05
sara-jr <salazar.ramirez.j@hotmail.com>
2023-08-26 12:47:50 -0600

    Arreglado el nombre de preventDefault, cambio a url de busqueda generada por nombre

--- pdv/urls.py
+++ pdv/urls.py
@@ -5,7 +5,7 @@ app_name = 'pdv'
 urlpatterns = [
     path('sell/', views.sell, name='sell'),
     path('search/', views.article_search),
-    path('activesearch/', views.active_search),
+    path('activesearch/', views.active_search, name='article_search'),
     path('makesale/', views.make_sale, name='makesale'),
     path('article/', views.article, name='article'),
     path('article/<int:id>/', views.article_get, name='article_existing'),
