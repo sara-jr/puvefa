66cf484e8c2867c84fe97afc8ebb21f87520764d
sara-jr <salazar.ramirez.j@hotmail.com>
2024-03-07 22:34:06 -0600

    Prueba para verificar si el cliente puede registrar recetas totales y parciales de ventas sin articulos controlados

--- pdv/tests/test_client_side_prescription.py
+++ pdv/tests/test_client_side_prescription.py
@@ -32,20 +32,11 @@ class PrescriptionClientSideTests(TestCase):
             has_iva=False,
             category=self.dummy_controlled_category,
         )
-        self.dummy_soldout_article = Article.objects.create(
-            name='Dummy soldout article',
-            description='Dummy article used for tests, quantity is zero',
-            barcode='0000000000003',
-            price=decimal.Decimal(30),
-            purchase_price=decimal.Decimal(20),
-            quantity=0,
-            min_quantity=1,
-            has_iva=False,
-            category=self.dummy_category,
-        )
         self.dummy_controlled_sale = Sale.objects.create(date=date.today(), amount_payed=self.dummy_controlled_article.price)
-        self.dummy_single_sale = SingleSale.objects.create(quantity=1, article=self.dummy_controlled_article, sale=self.dummy_controlled_sale)
+        self.dummy_controlled_single_sale = SingleSale.objects.create(quantity=1, article=self.dummy_controlled_article, sale=self.dummy_controlled_sale)
         self.dummy_medic = Medic.objects.create(name='Dummy', sur_name_a='Dummy', sur_name_b='Dummy', address='#123 Calle falsa', cedula='1234567890', ssa='1234567890')
+        self.dummy_sale = Sale.objects.create(date=date.today(), amount_payed=self.dummy_article.price)
+        self.dummy_single_sale = SingleSale.objects.create(quantity=1, article=self.dummy_article, sale=self.dummy_sale)
 
 
     def test_make_prescription_total(self):
@@ -90,3 +81,31 @@ class PrescriptionClientSideTests(TestCase):
         self.assertEqual(prescription.sale, self.dummy_controlled_sale, 'Sales do not match')
         self.assertEqual(prescription.date, self.dummy_controlled_sale.date, 'Dates do not match')
         self.assertEqual(prescription.medic, self.dummy_medic, 'Medics do not match')
+
+
+    def test_make_prescription_total_from_regular_sale(self):
+        """
+          Test if the client can register a total prescription from a sale that does not include controlled articles 
+        """
+        prescription_data = {
+            'type': 'total',
+            'sale': self.dummy_sale.id,
+            'medic': self.dummy_medic.id,
+        }
+        response = self.client.post(reverse('pdv:prescriptions'), prescription_data)
+        self.assertNotEqual(response.status_code, 200, 'Server acepted the prescription')
+        self.assertEqual(PrescriptionTotal.objects.count(), 0, 'Object was created')
+
+
+    def test_make_prescription_partial_from_regular_sale(self):
+        """
+          Test if the client can register a total prescription from a sale that does not include controlled articles 
+        """
+        prescription_data = {
+            'type': 'partial',
+            'sale': self.dummy_sale.id,
+            'medic': self.dummy_medic.id,
+        }
+        response = self.client.post(reverse('pdv:prescriptions'), prescription_data)
+        self.assertNotEqual(response.status_code, 200, 'Server acepted the prescription')
+        self.assertEqual(PrescriptionPartial.objects.count(), 0, 'Object was created')
