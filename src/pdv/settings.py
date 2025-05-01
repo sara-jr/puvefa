6900ee5dfdc00dbf2a504168c17c26301969a557
salazar.ramirez.j@hotmail.com <salazar.ramirez.j@hotmail.com>
2025-05-01 14:35:59 -0600

    Modificado pdv.settings para cargar la configuración del archivo .env

--- src/pdv/settings.py
+++ src/pdv/settings.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 
-CONTROLLED_CATEGORY_NAME = 'Antibiotico'
-ITEMS_PER_PAGE = 5
-MAX_SEARCH_RESULTS = 8
-MAX_PAYMENT_PER_SALE = 500_000
-PRINTER_NAME = 'epson-termal'
-RECIPT_DIR = '/tmp/'
+load_dotenv()
+
+# Ttest
+CONTROLLED_CATEGORY_NAME: str = 'Antibiotico'
+ITEMS_PER_PAGE: int = int(os.getenv('PUVEFA_ITEMS_PER_PAGE', 25))
+MAX_SEARCH_RESULTS: int = int(os.getenv('PUVEFA_MAX_SEARCH_RESULTS', 12))
+MAX_PAYMENT_PER_SALE: int = int(os.getenv('PUVEFA_MAX_PAYMENT_PER_SALE', 10_000))
+PRINTER_NAME: str = os.getenv('PUVEFA_PRINTER_NAME', 'termal-printer')
+RECIPT_DIR: str = os.getenv('PUVEFA_RECIPT_DIR', '/tmp')
